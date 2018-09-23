a4512c6afae0948a9bdd475c521211bbec8540b8
Lisa Kim <lisakeem@outlook.com>
2018-09-23 01:16:32 -0700

    added font-awesome packages

--- README.md
+++ README.md
@@ -19,3 +19,19 @@ npm run build
 ```
 npm run lint
 ```
+
+###### Set up Webstorm IDE to detect ESLint Errors:
+```
+1. Go to WebStorm Preferences
+2. Type "eslint" in the search bar
+3. Click on ESLint
+4. Click on Enable box
+5. Click Apply & OK
+```
+
+###### Change Webstorm's default 4 space/tabs to 2:
+```
+1. Go to Webstorm Preferences
+2. Type "code style" in the search bar
+3. Change space/tabs to 2 for "JavaScript" and "Other File Types"
+```
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+// import {  } from '@fortawesome/free-regular-svg-icons';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+
 import App from './App.vue';
 import router from './router';
+
+library.add(faCode, faGithub, faLinkedinIn);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
